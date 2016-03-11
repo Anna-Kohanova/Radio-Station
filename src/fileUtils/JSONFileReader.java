39d45248c3777904995caea9af5e6723b7dd9408
Anna-Kohanova <anitacx88@gmail.com>
2016-03-11 21:39:31 +0300

    Add files to work with JSON

---
+++ src/fileUtils/JSONFileReader.java
@@ -0,0 +1,27 @@
+package fileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public final class JSONFileReader {
+
+    private static File file;
+    private static FileInputStream stream;
+    
+    public static String readerFromJSON(String fileName) throws FileNotFoundException, IOException {
+        
+        file = new File(fileName);
+        if(!file.exists()) {
+            throw new FileNotFoundException(file.getName());
+        }
+        stream = new FileInputStream(file);
+        
+        byte[] data = new byte[(int) file.length()];
+        stream.read(data);
+        stream.close();
+        
+        return new String(data, "UTF-8");
+    }
+}

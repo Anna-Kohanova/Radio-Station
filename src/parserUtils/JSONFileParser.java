38992bce0d73334e6c801dba32d0e83de4bf1821
Anna-Kohanova <anitacx88@gmail.com>
2016-03-20 22:30:41 +0300

    Add song parser

--- src/parserUtils/JSONFileParser.java
+++ src/parserUtils/JSONFileParser.java
@@ -1,5 +1,21 @@
 package parserUtils;
 
+import java.util.ArrayList;
+import musicCollection.data.Song;
+import org.json.simple.JSONObject;
+
 public class JSONFileParser {
-    
+
+    public Song parseSongInfo(JSONObject songObject, Song song) {
+
+        song.setAlbum(songObject.get("album").toString());
+        song.setArtist(songObject.get("artist").toString());
+        song.setTitle(songObject.get("title").toString());
+        song.setYear(((Long) songObject.get("year")).intValue());
+        song.setPlaybacks(((Long) songObject.get("playbacks")).intValue());
+        song.setRating((Double) songObject.get("rating"));
+        song.setTags((ArrayList) songObject.get("tags"));
+
+        return song;
+    }
 }

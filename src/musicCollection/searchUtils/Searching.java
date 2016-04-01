f8c80d736a1fff0798bd376ed03f162042a8e95f
Anna-Kohanova <anitacx88@gmail.com>
2016-04-01 23:32:25 +0300

    Add searching by parameter

--- src/musicCollection/searchUtils/Searching.java
+++ src/musicCollection/searchUtils/Searching.java
@@ -1,5 +1,61 @@
 package musicCollection.searchUtils;
 
-public class Searching {
-    
+import java.util.ArrayList;
+import musicCollection.data.Song;
+
+public class Searching {  // class static? или передавать список всех песен в каждую функцию?
+
+    private ArrayList<Song> allSongsList;
+
+    public Searching(ArrayList<Song> allSongsList) {
+        this.allSongsList = allSongsList;
+    }
+
+    public ArrayList<Song> searchByArtist(String artist) {
+
+        ArrayList<Song> searchingSongsByParameter = new ArrayList<>();
+
+        for (Song song : allSongsList) {
+            if (artist.equalsIgnoreCase(song.getArtist())) {
+                searchingSongsByParameter.add(song);
+            }
+        }
+        return searchingSongsByParameter;
+    }
+
+    public ArrayList<Song> searchByYear(int year) {
+
+        ArrayList<Song> searchingSongsByParameter = new ArrayList<>();
+
+        for (Song song : allSongsList) {
+            if (year == song.getYear()) {
+                searchingSongsByParameter.add(song);
+            }
+        }
+        return searchingSongsByParameter;
+    }
+
+    public ArrayList<Song> searchByTag(String tag) {
+
+        ArrayList<Song> searchingSongsByParameter = new ArrayList<>();
+
+        for (Song song : allSongsList) {
+            if (song.getTags().contains(tag.toLowerCase())) {
+                searchingSongsByParameter.add(song);
+            }
+        }
+        return searchingSongsByParameter;
+    }
+
+    public ArrayList<Song> searchByRating(Double rating) {
+        
+        ArrayList<Song> searchingSongsByParameter = new ArrayList<>();
+        
+        for (Song song : allSongsList) {
+            if (rating == song.getRating()) {
+                searchingSongsByParameter.add(song);
+            }
+        }
+        return searchingSongsByParameter;
+    }
 }
